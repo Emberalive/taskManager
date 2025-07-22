5d7f70617b1c073081fe3d886699e03e729ad63f
Sammy Boy <samueltsutcliffe@gmail.com>
2025-07-22 13:24:19 +0100

    I have refactored the login function, and split it into two functions, a handleLogin and a login function, the handleLogin function extracts all the data from the form and rus the login function, which sends a GET request to the api, to check the uers details. and then added a register function, that sends a POST request to create a user in the database and checks if all the form data is there, from all fields. I then check if he response from the api, sends a truthy value in the registered attribute of the obect returned/

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
+
 export default function Login (props) {
+    const [isRegistering, setIsRegistering] = useState(false);
+
+    function clickRegister() {
+        setIsRegistering(prev => !prev);
+    }
 
     async function handleLogin (event) {
+        console.log("handle login called")
         event.preventDefault()
         let resData = null
         const formData = event.target;
 
         const password = formData.password.value;
-        const username = formData.username.value;
+        const username = formData.user.value;
+        await login(username, password);
+    }
 
+    async function login (username, password) {
+        console.log("login called")
+        let resData = {}
         if (username && password) {
             console.log("sending login request")
-            await fetch(`http://localhost:7000/login?username=${encodeURIComponent(username)}&password=${encodeURIComponent(password)}`, {method: 'GET'})
-            .then(res => res.json())
-            .then(data => {resData = data})
-            .catch(err => console.log("This is an error: \n" + err));
+            await fetch(`http://localhost:7000/login?
+            &username=${encodeURIComponent(username)}
+            &password=${encodeURIComponent(password)}`, {method: 'GET'})
+                .then(res => res.json())
+                .then(data => {resData = data})
+                .catch(err => console.log("This is an error: \n" + err));
         }
 
         if (resData) {
@@ -23,15 +38,43 @@ export default function Login (props) {
         }
     }
 
+    async function handleRegister (event) {
+        console.log("register function called")
+        event.preventDefault()
+        const formData = event.target;
+        const password = formData.password.value;
+        const username = formData.user.value;
+        const confirmPassword = formData.confirm.value;
+
+        if (username && password && confirmPassword) {
+            const response = await fetch(`http://localhost:7000/register`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                    confirmPassword: confirmPassword,
+                })
+            })
+            const data = await response.json()
+            if (data.registered) {
+                await login(username, password)
+            }
+        }
+    }
+
     return(
         <section className="Login_page">
             <div className="Login_page__container">
-                <h1>Please Login</h1>
-                <form className="Login_page__form" onSubmit={handleLogin}>
-                    <input type="text" placeholder="Username" name="username" />
+                <h1>{isRegistering ? "Please register" : "Please Login"}</h1>
+                <form className="Login_page__form" onSubmit={isRegistering ? handleRegister : handleLogin}>
+                    <input type="text" placeholder="Username" name="user" />
                     <input type="password" placeholder="Password" name="password" />
+                    {isRegistering && <input type="password" placeholder="Confirm Password" name="confirm"/>}
                     <div className="login_page__links">
-                        <a href="#">register</a>
+                        <a onClick={clickRegister} href="#">{isRegistering? "Login": "Register"}</a>
                     </div>
                     <button type="submit" className="Login_page__submit">Login</button>
                 </form>
