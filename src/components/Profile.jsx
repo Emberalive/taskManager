8985ed9dbd42181635618ac9c3e241ced1967459
Sammy Boy <samueltsutcliffe@gmail.com>
2025-07-23 16:24:01 +0100

    I have added the ability to edit the profile, by adding state that indicates when te user is editing their profuile. i adding a conditional render that will change the p tags and the h2 elements to be rendered as input text type and testarea

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function Profile(props) {
+    const [editProfile, setEditProfile] = useState(false);
+
+
     return (
         <div className="profile">
             <h1>Welcome - {props.user.username}</h1>
@@ -9,10 +14,22 @@ export default function Profile(props) {
                     className="avatar"
                 />
                 <div className="details">
-                    <h2>{props.user.username}</h2>
-                    <p>Email: {props.user.email}</p>
-                    <p>Bio: {props.user.bio}</p>
+                    {editProfile ? <input type={"text"}
+                                          name={"Username"}
+                                          value={props.user.username}
+                                          placeholder={"username"}
+                                          /> :
+                        <h2>{props.user.username}</h2>}
+
+                    {editProfile? <input type={"text"} name={"email"} placeholder={"Email"}/> :
+                        <p>Email: {props.user.email}</p>}
+
+                    {editProfile? <textarea name={"bio"} placeholder={"Bio"}/> :
+                        <p>Bio: {props.user.bio}</p>}
                 </div>
+                <button onClick={() => {
+                    setEditProfile(prevState => !prevState);
+                }}>{editProfile? "Save" : "Edit"}</button>
             </div>
         </div>
     );
