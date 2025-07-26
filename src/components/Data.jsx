7d042ccbf6ff562bc7fc26baf008b294a9706bcb
Sammy Boy <samueltsutcliffe@gmail.com>
2025-07-26 22:17:32 +0100

    I have officially managed to make the task look a lot moree appealing with no help from anything on the internet, thats it, no note no nothing, I HATE CSS

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,25 +1,20 @@
 export default function Data (props) {
     return (
         <div className="data-container">
-            <div>
                 <header className="task-header">
                     {props.isEditingID === props.task.id ?
-                        <textarea
+                        <input type={"text"}
                             onChange={(e) => {
                                 props.setEditTitle(e.target.value)
                             }
                             }
                             defaultValue={props.task.title}
-                        ></textarea> :
+                        ></input> :
                         <h2 className="task-title">{props.task.title}</h2>
                     }
-                </header>
-                <section className="data">
-                    <h2>Details</h2>
                     <p className="date">{props.task.date}</p>
                     {props.deleteFailed &&<h3>Task failed to delete</h3>}
-                </section>
-            </div>
+                </header>
             {props.isEditingID === props.task.id ?
                 <textarea
                     onChange={(e) => {
